0fb7b1b63ecb5568b612ee6368805c3e1b8fd1f9
V-inTim <vinichuk.timofey@mail.ru>
2024-11-06 23:48:21 +0300

    Add serializers.

--- taxiunn/client_auth/serializers.py
+++ taxiunn/client_auth/serializers.py
@@ -17,3 +17,46 @@ class ClientSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+
+class PasswordRecoverySerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(write_only=True)
+
+    class Meta:
+        model = Client
+        fields = ['email']
+
+    def validate_email(self, value):
+        # Проверка существования модели с таким email
+        if not Client.objects.filter(email=value).exists():
+            raise serializers.ValidationError(("An account with "
+                                               "this email does not exist."))
+
+        return value
+
+
+class PasswordRecoveryVerifySerializer(PasswordRecoverySerializer):
+
+    verification_code = serializers.IntegerField(write_only=True)
+
+    class Meta(PasswordRecoverySerializer.Meta):
+        fields = PasswordRecoverySerializer.Meta.fields + ['verification_code']
+
+
+class PasswordRecoveryChangeSerializer(PasswordRecoverySerializer):
+
+    password = serializers.IntegerField(write_only=True)
+
+    class Meta(PasswordRecoverySerializer.Meta):
+        fields = PasswordRecoverySerializer.Meta.fields + ['password']
+
+    def save(self):
+        email = self.validated_data.get('email')
+        password = self.validated_data.get('password')
+
+        user = Client.objects.get(email=email)
+        user.set_password(str(password))
+
+        user.save()
+        return user
