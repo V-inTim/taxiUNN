6478c5f165a51dc7cb0817f8f8debcd003fe3be0
V-inTim <vinichuk.timofey@mail.ru>
2024-11-25 20:22:58 +0300

    Add Client view class.

--- taxiunn/client_profile/views.py
+++ taxiunn/client_profile/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ClientView(APIView):
+    """View клиентских данных."""
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response(
+            user.get_data(),
+            status=status.HTTP_200_OK,
+        )
+
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response(
+            {'message': 'User account deleted'},
+            status=status.HTTP_200_OK,
+        )
