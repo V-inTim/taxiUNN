985333f9f407f68478eb23367b3bf94ce2c075c4
V-inTim <vinichuk.timofey@mail.ru>
2024-11-25 21:41:01 +0300

    Change typing.

--- taxiunn/admin_auth/models.py
+++ taxiunn/admin_auth/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-from .models import Admin
-
 
 class AdminManager(BaseUserManager):
     """Специфика Django.
@@ -11,7 +9,7 @@ class AdminManager(BaseUserManager):
     класс Manager.
     """
 
-    def create_user(self, email: str, password: str) -> Admin:
+    def create_user(self, email: str, password: str):
         """Создает и возвращает администратора с имэйлом, паролем."""
         if email is None:
             raise TypeError('Clients must have an email address.')
@@ -25,7 +23,7 @@ class AdminManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email: str, password: str) -> Admin:
+    def create_superuser(self, email: str, password: str):
         """Создает и возвращает администратора с привилегиями суперадмина."""
         user = self.create_user(email, password)
         user.make_superuser()
