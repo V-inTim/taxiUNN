4a9b5b346fc0a36b4df9d8655c8a9c61a574823a
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2024-11-28 12:58:29 +0300

    Revert "Settings."

--- taxiunn/client_auth/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClientAuthConfig(AppConfig):
-    """Определяет конфигурацию для Django-приложения."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "client_auth"
--- taxiunn/driver_auth/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class DriverAuthConfig(AppConfig):
-    """Определение конфигурации Django-приложения."""
-
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'driver_auth'
