6e337494f9a0996d2358cd9555ff95085a6ed2c3
V-inTim <vinichuk.timofey@mail.ru>
2024-12-06 00:42:02 +0300

    Code style.

--- taxiunn/admin_auth/apps.py
+++ taxiunn/admin_auth/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class AdminAuthConfig(AppConfig):
+    """App config."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_auth'
--- taxiunn/taxi_fare/apps.py
+++ taxiunn/taxi_fare/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class TaxiFareConfig(AppConfig):
+    """App config."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'taxi_fare'
