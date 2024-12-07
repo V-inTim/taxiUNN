335dbd558c2cc54a416b8da3200fb1882f1906d0
V-inTim <vinichuk.timofey@mail.ru>
2024-12-07 16:52:31 +0300

    Add views for TaxiFare.

--- taxiunn/taxi_fare/views.py
+++ taxiunn/taxi_fare/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    RetrieveUpdateDestroyAPIView,
+    CreateAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import (
+    TaxiFareSerializer,
+    TaxiFareCreateSerializer,
+)
+from .models import TaxiFare
+
+
+class TaxiFareCreateView(CreateAPIView):
+    """View for create taxi fare."""
+
+    queryset = TaxiFare.objects.all()
+    serializer_class = TaxiFareCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class TaxiFareView(RetrieveUpdateDestroyAPIView):
+    """View for retrieve, update and remove taxi fare."""
+
+    queryset = TaxiFare.objects.all()
+    serializer_class = TaxiFareSerializer
+    permission_classes = [IsAuthenticated]
