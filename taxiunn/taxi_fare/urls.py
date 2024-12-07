0f2a55031297d6794a944108e8e402396d9a9377
V-inTim <vinichuk.timofey@mail.ru>
2024-12-07 16:59:19 +0300

    Add list view for TaxiFare.

--- taxiunn/taxi_fare/urls.py
+++ taxiunn/taxi_fare/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     TaxiFareView,
     TaxiFareCreateView,
+    TaxiFareListView,
 )
 
 urlpatterns = [
     path('<int:pk>/', TaxiFareView.as_view(), name="taxi_fare"),
     path('', TaxiFareCreateView.as_view(), name="taxi_fare_create"),
+    path('list/', TaxiFareListView.as_view(), name="taxi_fare_list"),
 ]
