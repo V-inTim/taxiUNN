60dd4712b1a14616a1c783fcfefe5e721cbd2ab2
V-inTim <vinichuk.timofey@mail.ru>
2024-12-07 16:41:18 +0300

    Add model Taxi Fare.

--- taxiunn/taxi_fare/models.py
+++ taxiunn/taxi_fare/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class TaxiFare(models.Model):
+    """Model Class for taxi fare."""
+
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        """Model string."""
+        return self.name
